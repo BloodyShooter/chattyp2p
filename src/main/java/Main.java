300d57c9595021c2363b4b9a544cad86815c01d2
Egor <gvozdetscky.egor@yandex.ru>
2018-03-29 10:24:30 +0400

    Работает консольное приложение

---
+++ src/main/java/Main.java
@@ -0,0 +1,48 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Name: ");
+		String name = scanner.nextLine();
+
+		System.out.print("Source port: ");
+		int sourcePort = Integer.parseInt(scanner.nextLine());
+
+		System.out.print("Destination IP: ");
+		String destinationIP = scanner.nextLine();
+
+		System.out.print("Destination port: ");
+		int destinationPort = Integer.parseInt(scanner.nextLine());
+
+		Channel channel = new Channel();
+		channel.bind(sourcePort);
+		channel.start();
+		System.out.println("Started.");
+
+		InetSocketAddress address = new InetSocketAddress(destinationIP, destinationPort);
+
+		while (true) {
+			String msg = scanner.nextLine();
+
+			if (msg.isEmpty()) {
+				break;
+			}
+
+			msg = name + " >> " + msg;
+
+			channel.sendTo(address, msg);
+			System.out.println(msg);
+		}
+
+		scanner.close();
+		channel.stop();
+
+		System.out.println("closed.");
+	}
+}
